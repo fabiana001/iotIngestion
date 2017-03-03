f1a9c2c390bec00999e18cd4cab1b14758bf4afa
fabiana <fabiana.lan@hotmail.it>
2017-03-03 16:22:20 +0100

    code review

--- src/main/scala/it/teamDigitale/avro/EventAvroConverter.scala
+++ src/main/scala/it/teamDigitale/avro/EventAvroConverter.scala
@@ -14,13 +14,3 @@ object EventAvroConverter {
   implicit private val specificAvroBinaryInjection: Injection[Event, Array[Byte]] = SpecificAvroCodecs.toBinary[Event]
   def convert(event: Event): Array[Byte] = specificAvroBinaryInjection(event)
 }
-
-object ExampleAvroConverter {
-  val parser = new Schema.Parser()
-  val USER_SCHEMA = scala.io.Source.fromFile("/Users/fabiana/Downloads/kafka-camel-example/src/main/scala/it/teamDigitale/avro/Event.avsc").mkString
-  val schema = parser.parse(USER_SCHEMA)
-
-  implicit private val specificAvroBinaryInjection: Injection[Example, Array[Byte]] = SpecificAvroCodecs.toBinary[Example]
-  def convert(event: Example): Array[Byte] = specificAvroBinaryInjection(event)
-}
-
