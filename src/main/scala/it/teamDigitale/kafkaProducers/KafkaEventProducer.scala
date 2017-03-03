dfe3f35453b6231c26e787a5605b8d9678604b84
fabiana <fabiana.lan@hotmail.it>
2017-03-03 16:27:25 +0100

    code review

--- src/main/scala/it/teamDigitale/kafkaProducers/KafkaEventProducer.scala
+++ src/main/scala/it/teamDigitale/kafkaProducers/KafkaEventProducer.scala
@@ -3,23 +3,22 @@ package it.teamDigitale.kafkaProducers
 import java.io.Serializable
 import java.util.Properties
 
-import it.teamDigitale.avro.{ Example, ExampleAvroConverter }
 import it.teamDigitale.kafkaProducers.eventConverters.EventConverter
 import org.apache.kafka.clients.producer.{ KafkaProducer, ProducerRecord }
-import org.slf4j.LoggerFactory
+import org.slf4j.{ Logger, LoggerFactory }
 
 import scala.reflect.{ ClassTag, classTag }
 import scala.util.{ Failure, Try }
 
 /**
- * Created with <3 by Team Digitale
- *
- * It sends events to a kafka queue
- */
+  * Created with <3 by Team Digitale
+  *
+  * It sends events to a kafka queue
+  */
 class KafkaEventProducer[T <: EventConverter: ClassTag](props: Properties, topic: String) {
 
   val producer = new KafkaProducer[Array[Byte], Array[Byte]](props)
-  val logger = LoggerFactory.getLogger(this.getClass)
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
   val converter: T = classTag[T].runtimeClass.newInstance().asInstanceOf[T with Serializable]
 
   def run(time: Long): Long = {
@@ -39,16 +38,9 @@ class KafkaEventProducer[T <: EventConverter: ClassTag](props: Properties, topic
 
   def exec(bytes: Array[Byte]): Try[Unit] = Try {
     val message = new ProducerRecord[Array[Byte], Array[Byte]](topic, bytes)
-    println(message)
     producer.send(message)
     ()
   }
-
-//  def example() = {
-//    val avro = ExampleAvroConverter.convert(Example("ciao"))
-//    exec(avro)
-//    producer.flush()
-//  }
 
   def close(): Unit = producer.close()
 
