80fdf617faa92263686a6033acaff23ee00e18cb
fabiana <fabiana.lan@hotmail.it>
2017-03-03 16:21:31 +0100

    added working config application

--- src/main/scala/it/teamDigitale/consumers/sparkStreaming/ConsumerMain.scala
+++ src/main/scala/it/teamDigitale/consumers/sparkStreaming/ConsumerMain.scala
@@ -8,15 +8,15 @@ import it.teamDigitale.avro.Event
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{ DataType, StructType }
-import org.apache.spark.streaming.{ Seconds, StreamingContext }
-import org.slf4j.LoggerFactory
-
+import org.apache.spark.streaming.{ Minutes, Seconds, StreamingContext }
+import org.slf4j.{ Logger, LoggerFactory }
 
 /**
- * Created by fabiana on 28/02/17.
+ * Created with <3 by Team Digitale.
  */
 object ConsumerMain {
-  val logger = LoggerFactory.getLogger(this.getClass)
+
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   def main(args: Array[String]): Unit = {
 
@@ -24,12 +24,13 @@ object ConsumerMain {
 
     var config = ConfigFactory.load()
 
+    val filename = config.getString("spark-dataIngestion-example.hdfs.filename")
+
     val test = args match {
       case Array(testMode: String) =>
-        logger.info(s"kafka: ${config.getString("spark-opentsdb-exmaples.kafka.bootstrapServers")}")
-        logger.info(s"zookeeper: ${config.getString("spark-opentsdb-exmaples.zookeeper.host")}")
+        logger.info(s"kafka: ${config.getString("spark-dataIngestion-example.kafka.bootstrapServers")}")
+        logger.info(s"zookeeper: ${config.getString("spark-dataIngestion-example.zookeeper.host")}")
         testMode.toBoolean
-
       case _ => true
     }
 
@@ -37,33 +38,28 @@ object ConsumerMain {
       spark.master("local[*]")
 
     val sparkSession = spark
-      //.enableHiveSupport()
       .getOrCreate()
 
     val sparkConf = sparkSession.sparkContext
-    implicit val ssc = new StreamingContext(sparkConf, Seconds(5))
+    implicit val ssc = new StreamingContext(sparkConf, Minutes(3))
 
-    val topic = ConfigFactory.load().getString("spark-opentsdb-exmaples.kafka.topic")
-    val servers = ConfigFactory.load().getString("spark-opentsdb-exmaples.kafka.bootstrapServers")
-    val deserializer = ConfigFactory.load().getString("spark-opentsdb-exmaples.kafka.deserializer")
+    val topic = ConfigFactory.load().getString("spark-dataIngestion-example.kafka.topic")
+    val servers = ConfigFactory.load().getString("spark-dataIngestion-example.kafka.bootstrapServers")
+    val deserializer = ConfigFactory.load().getString("spark-dataIngestion-example.kafka.deserializer")
 
     val props = Map(
-      // "metadata.broker.list" -> brokers,
       "bootstrap.servers" -> servers,
       "key.deserializer" -> classOf[ByteArrayDeserializer],
       "value.deserializer" -> classOf[ByteArrayDeserializer],
       //"auto.offset.reset" -> "earliest",
       "enable.auto.commit" -> (false: java.lang.Boolean),
-      "group.id" -> "test_event_consumer"
-    )
+      "group.id" -> "test_event_consumer")
 
     val eventConsumer = new EventConsumer(ssc, Set(topic), props)
-    val stream = eventConsumer.getEvents()
+    val stream = eventConsumer.getEvents
 
     stream.print(100)
-    stream.foreachRDD(rdd => eventConsumer.saveAsParquet(rdd, spark.getOrCreate(), "pippo"))
-
-    //eventConsumer.saveAsParquet(ev, spark.getOrCreate(), "ciao", List())
+    stream.foreachRDD(rdd => eventConsumer.saveAsParquet(rdd, spark.getOrCreate(), filename, List("ts")))
 
     ssc.start()
     ssc.awaitTermination()
