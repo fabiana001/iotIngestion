bb190bb3ece4ea9cabfeb9c1bcbec132b1638b08
fabiana <fabiana.lan@hotmail.it>
2017-03-03 16:26:26 +0100

    code review

--- src/main/scala/it/teamDigitale/consumers/sparkStreaming/EventConsumer.scala
+++ src/main/scala/it/teamDigitale/consumers/sparkStreaming/EventConsumer.scala
@@ -9,27 +9,28 @@ import com.twitter.bijection.avro.SpecificAvroCodecs
 import com.typesafe.config.ConfigFactory
 import it.teamDigitale.avro.Event
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka010.KafkaUtils
-import org.slf4j.LoggerFactory
+import org.slf4j.{Logger, LoggerFactory}
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
+import scala.reflect.ClassTag
 import scala.util.Success
 
 /**
- * Created by fabiana on 23/02/17.
- */
+  * Created with <3 by Team Digitale.
+  */
 class EventConsumer(
-    @transient val ssc: StreamingContext,
-    topicSet: Set[String],
-    kafkaParams: Map[String, Object]
-) extends Serializable {
-  val logger = LoggerFactory.getLogger(this.getClass)
+                     @transient val ssc: StreamingContext,
+                     topicSet: Set[String],
+                     kafkaParams: Map[String, Object]
+                   ) extends Serializable {
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-  def getEvents(): DStream[Event] = {
+  def getEvents: DStream[Event] = {
 
     logger.info("Consumer is running")
 
@@ -50,35 +51,21 @@ class EventConsumer(
   }
 
   def saveAsParquet(rdd: RDD[Event], spark: SparkSession, filename: String, attributes: List[String] = List()): Unit = {
-    //val sqlContext: SQLContext = spark.sqlContext
-
 
     if (!rdd.isEmpty()) {
-
-      val props = Introspector.getBeanInfo(classOf[Event]).getPropertyDescriptors
-      props.foreach { prop =>
-        println(prop.getDisplayName())
-        println("\t" + prop.getReadMethod())
-        println("\t" + prop.getWriteMethod());
-      }
-
-    //  val df: DataFrame = spark.createDataFrame(rdd, classOf[Event])
-    //  val a: StructType = DataType.fromJson(Event.SCHEMA$.toString).asInstanceOf[StructType]
-      //val pippo = sqlContext.createDataFrame(rdd.toJavaRDD(), classOf[Event])
-
       import spark.implicits._
       val df = rdd.toDF()
-      df.write.partitionBy(attributes: _*).parquet(filename)
+      df.write.mode("append").partitionBy(attributes: _*).parquet(filename)
     }
 
   }
 
   def saveAsAvro(rdd: RDD[Array[Byte]], spark: SparkSession, filename: String, attributes: List[String] = List()): Unit = {
-    val sqlContext = spark.sqlContext
-    val schema = SchemaConverters.toSqlType(Event.SCHEMA$)
-    val df = spark.createDataFrame(rdd.toJavaRDD(), classOf[Event]).toDF()
-    df.write.partitionBy(attributes: _*).avro(filename)
-
+    if (!rdd.isEmpty()) {
+      import spark.implicits._
+      val df: DataFrame = rdd.toDF()
+      df.write.mode("append").partitionBy(attributes: _*).avro(filename)
+    }
   }
 
 }
