b2dff2dad0305fe321123cb00ddb2e7d92aa791f
fabiana <fabiana.lan@hotmail.it>
2017-03-16 16:40:39 +0100

    indent commit

--- src/main/scala/it/teamDigitale/timeseriesdatabases/influxdb/InfluxDbClient.scala
+++ src/main/scala/it/teamDigitale/timeseriesdatabases/influxdb/InfluxDbClient.scala
@@ -4,15 +4,15 @@ import java.util
 import java.util.concurrent.TimeUnit
 
 import collection.JavaConverters._
-import it.teamDigitale.avro.{DataPoint, Event}
+import it.teamDigitale.avro.{ DataPoint, Event }
 import org.apache.spark.rdd.RDD
 import org.influxdb.InfluxDB.ConsistencyLevel
-import org.influxdb.dto.{BatchPoints, Point}
-import org.influxdb.{InfluxDB, InfluxDBFactory}
+import org.influxdb.dto.{ BatchPoints, Point }
+import org.influxdb.{ InfluxDB, InfluxDBFactory }
 
 /**
-  * Created by fabiana on 06/03/17.
-  */
+ * Created by fabiana on 06/03/17.
+ */
 class InfluxDbClient(user: String, password: String, url: String = "http://localhost:8086") {
 
   //TODO define best values for FLUSH_PERIOD and BATCH_SIZE
@@ -25,27 +25,26 @@ class InfluxDbClient(user: String, password: String, url: String = "http://local
 
   def read(): RDD[DataPoint] = ???
 
-
   def write(rdd: Iterator[DataPoint], database: String = "timeseries"): Unit = {
 
-      val batchPoints = BatchPoints
-        .database(database)
-        .tag("async", "true")
-        .consistency(ConsistencyLevel.ONE)
-        .retentionPolicy("autogen")
-        .build()
+    val batchPoints = BatchPoints
+      .database(database)
+      .tag("async", "true")
+      .consistency(ConsistencyLevel.ONE)
+      .retentionPolicy("autogen")
+      .build()
 
-      rdd.foreach{ dp =>
-        val fields: util.Map[String, Object] = dp.values.map { case (k, v) => (k, v.asInstanceOf[Object]) }.asJava
-        val point = Point.measurement(dp.host)
-          .time(dp.ts, TimeUnit.MILLISECONDS)
-          .fields(fields)
-          .tag(dp.tags.asJava)
-          .build()
-        batchPoints.point(point)
-      }
-      influxDB.write(batchPoints)
+    rdd.foreach { dp =>
+      val fields: util.Map[String, Object] = dp.values.map { case (k, v) => (k, v.asInstanceOf[Object]) }.asJava
+      val point = Point.measurement(dp.host)
+        .time(dp.ts, TimeUnit.MILLISECONDS)
+        .fields(fields)
+        .tag(dp.tags.asJava)
+        .build()
+      batchPoints.point(point)
+    }
+    influxDB.write(batchPoints)
   }
 
-  def close() =  influxDB.close()
+  def close() = influxDB.close()
 }
