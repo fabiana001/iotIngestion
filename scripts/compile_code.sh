6d2d576c5dcc9e3b69f552e082c62d63b6955c77
fabiana <fabiana.lan@hotmail.it>
2017-03-16 17:35:19 +0100

    added script files

--- scripts/compile_code.sh
+++ scripts/compile_code.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 cd ..
-rm -rf ./iotIngestion
+rm -rf ./iotingestion-1.0
 echo "-------------------------------------------"
 echo "             Compiling Code"
 echo "-------------------------------------------"
 sbt clean compile package universal:package-bin
-mkdir iotIngestion
-mv ./target/scala-2.11/iotingestion_2.11-1.0.jar ./iotIngestion/
-mv ./target/universal/iotingestion-1.0.zip ./iotIngestion/
-unzip ./iotIngestion/iotingestion-1.0.zip -d ./iotIngestion/
-rm ./iotIngestion/iotingestion-1.0.zip
+
+mv ./target/universal/iotingestion-1.0.zip ./
+
+unzip ./iotingestion-1.0.zip
+
+rm ./iotingestion-1.0.zip
+
+mv ./target/scala-2.11/iotingestion_2.11-1.0.jar ./iotingestion-1.0
 echo "------------------------------------------------------------------------------------------------"
-echo "     Code compiled at the forder $PWD/iotIngestion"
+echo "     Code compiled at the forder $PWD/iotingestion-1.0"
 echo "------------------------------------------------------------------------------------------------"
